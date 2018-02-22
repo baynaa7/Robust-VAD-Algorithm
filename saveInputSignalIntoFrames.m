94470b8f34224864c70c4c52d736ed471b0a5641
baynaa <iovlstdy@gmail.com>
2018-02-22 23:46:49 +0900

    launching robust mono/stereo channel VAD algorithm

--- saveInputSignalIntoFrames.m
+++ saveInputSignalIntoFrames.m
@@ -1,12 +1,9 @@
 function [xFrameN] = saveInputSignalIntoFrames(x1,winLen,shiftLen,TotFrames,numSamples)
-
     indx = 0;
     counter = 1;
     xFrameN = zeros(winLen,TotFrames);
-    while indx + winLen <= numSamples    
-    %     counter
+    while indx + winLen <= numSamples   
         xFrameN(:,counter) = (x1(indx+1:indx+winLen));
-        % update the indexes
         indx = indx + shiftLen;
         counter = counter+1;
     end
